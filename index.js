ee4f360763d875cbecacc445d300e59ab38b313e
d27sa <9d27sa@gmail.com>
2018-12-28 18:05:54 +0800

    1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// alert("Hello World!")
+$(function(){
+    $("#add").click(function(){
+        let v=$("#addtext").val();
+        let e=$("<li></li>").text(v);
+        let b=$("<button></button>").click(function(){
+            $(this).parent().remove();
+        })
+        b.text("x");
+        e.append(b);
+        $("#todo").append(e);
+        $("#addtext").val("");
+    })
+});
